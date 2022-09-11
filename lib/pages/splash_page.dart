6a51f978e7d4e6361e9e676821ea70055dc1a841
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-09-11 18:32:02 -0300

    Simulando espera da splash e chamando login page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:login_shared_prefs/routes/routes.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2)).then(
+        (value) => Navigator.of(context).pushReplacementNamed(Routes.LOGIN));
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: const Center(
+          child: CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
