158279bbf2c236a9b115ca352a35401b091e9b7c
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-09-11 18:42:48 -0300

    logica para controller

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,1 +1,14 @@
-class LoginController {}
+class LoginController {
+  String? _login;
+  setLogin(String login) => _login = login;
+
+  String? _pass;
+  setSenha(String pass) => _pass = pass;
+
+  Future<bool> auth() async {
+    if (_login == 'adm' && _pass == '123') {
+      return true;
+    }
+    return false;
+  }
+}
